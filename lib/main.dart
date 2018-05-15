e8eb7ea6f41bb676bd6bf822151831ee18a57c33
Nils Backe <nils.backe@gmail.com>
2018-05-15 19:59:56 -0400

    added tab view

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,12 @@ import 'package:map_view/map_view.dart';
 import 'package:http/http.dart' as http;
 
 var apiKey = "AIzaSyCEyNI6shSh4cpI3Ne6jQBxqTBGzBr4Kz0";
-String _apiType = "all_day";
+String _apiRange = "day";
+String _apiType = "all";
 
 void main() {
   MapView.setApiKey(apiKey);
-  runApp(new MaterialApp(
-    title: "Earthquake Data Mapper",
-    home: new Home(),
-  ));
+  runApp(new Home());
 }
 
 class Home extends StatefulWidget {
@@ -21,15 +19,21 @@ class Home extends StatefulWidget {
   _HomeState createState() => new _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   var _mapView = new MapView();
+  TabController _tabController;
 
   Map _data = new Map();
   List _features = new List();
 
-  int radioValue = 0;
+  int radioValue = 1;
+  int currentTab = 0;
 
-  _HomeState() {
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: 2, initialIndex: 0);
+    _tabController.addListener(_handleTabSelection);
     getQuakes().then((val) {
       setState(() {
         _data = val;
@@ -38,19 +42,25 @@ class _HomeState extends State<Home> {
     });
   }
 
-  void handleRadioValueChanged(int value) {
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _handleRadioValueChanged(int value) {
     setState(() {
       radioValue = value;
     });
     switch (radioValue) {
       case 0:
-        _apiType = "all_hour";
+        _apiRange = "hour";
         break;
       case 1:
-        _apiType = "all_day";
+        _apiRange = "day";
         break;
       case 2:
-        _apiType = "all_week";
+        _apiRange = "week";
         break;
       default:
     }
@@ -62,94 +72,143 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void _handleTabSelection() {
+    setState(() {
+      switch (_tabController.index) {
+        case 0:
+          _apiType = "all";
+          break;
+        case 1:
+          _apiType = "significant";
+          break;
+        default:
+      }
+    });
+    getQuakes().then((val) {
+      setState(() {
+        _data = val;
+        _features = _data['features'];
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Earthquake Data Mapper"),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      body: new Container(
-        // padding: new EdgeInsets.only(top: 30.0),
-        
-        child: new Column(
-          children: <Widget>[
-            new Padding(
-              padding: const EdgeInsets.all(2.0),
-            ),
-            new Row(
-              children: <Widget>[
-                new Radio<int>(
-                  value: 0,
-                  groupValue: radioValue,
-                  onChanged: handleRadioValueChanged,
-                  activeColor: Colors.white,
-                ),
-                new Text("Past Hour"),
-                new Radio<int>(
-                  value: 1,
-                  groupValue: radioValue,
-                  onChanged: handleRadioValueChanged,
-                  activeColor: Colors.white,
-                ),
-                new Text("Past Day"),
-                new Radio<int>(
-                  value: 2,
-                  groupValue: radioValue,
-                  onChanged: handleRadioValueChanged,
-                  activeColor: Colors.white,
-                ),
-                new Text("Past Week"),
-              ],
-            ),
-            new Padding(
-              padding: const EdgeInsets.all(10.0),
-            ),
-            new RaisedButton(
-              child: new Text("Show Map"),
-              onPressed: showMap,
-            ),
-            new Padding(padding: const EdgeInsets.all(10.0)),
-            new Flexible(
-              child: new ListView.builder(itemBuilder: (_, index) {
-                return quakeCard(_, index);
-              }),
-            ),
-          ],
+    return new MaterialApp(
+      title: "Earthquake Data Mapper",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Earthquake Data Mapper"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          bottom: new TabBar(
+            controller: _tabController,
+            tabs: <Widget>[
+              new Tab(
+                text: "All Earthquakes",
+              ),
+              new Tab(
+                text: "Significant Earthquakes",
+              ),
+            ],
+          ),
         ),
+        body: new TabBarView(
+          children: <Widget>[
+            getTabView(),
+            getTabView(),
+          ],
+          controller: _tabController,
+        ),
+        backgroundColor: Colors.blue,
       ),
-      backgroundColor: Colors.blue,
+    );
+  }
+
+  Widget getTabView() {
+    return new Container(
+      // padding: new EdgeInsets.only(top: 30.0),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Padding(
+            padding: const EdgeInsets.all(2.0),
+          ),
+          new Row(
+            children: <Widget>[
+              new Radio<int>(
+                value: 0,
+                groupValue: radioValue,
+                onChanged: _handleRadioValueChanged,
+                activeColor: Colors.white,
+              ),
+              new Text("Past Hour"),
+              new Radio<int>(
+                value: 1,
+                groupValue: radioValue,
+                onChanged: _handleRadioValueChanged,
+                activeColor: Colors.white,
+              ),
+              new Text("Past Day"),
+              new Radio<int>(
+                value: 2,
+                groupValue: radioValue,
+                onChanged: _handleRadioValueChanged,
+                activeColor: Colors.white,
+              ),
+              new Text("Past Week"),
+            ],
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(10.0),
+          ),
+          new RaisedButton(
+            child: new Text("Show Map"),
+            onPressed: showMap,
+          ),
+          new Padding(padding: const EdgeInsets.all(10.0)),
+          new Flexible(
+            child: new ListView.builder(
+              itemCount: _features.length,
+              itemBuilder: (_, index) {
+              return quakeCard(_, index);
+            }),
+          ),
+        ],
+      ),
     );
   }
 
   Card quakeCard(BuildContext context, int i) {
-    if (_features.length != 0) {
-      var mag = _features[i]['properties']['mag'];
+    if (_features == null || _features.isEmpty || _features.length == 0) {
       return new Card(
         color: Colors.white,
-        child: new Column(
-          children: <Widget>[
-            new ListTile(
-              leading: new Opacity(
-                opacity: mag / 10,
-                child: new CircleAvatar(
-                  backgroundColor: Colors.red[900],
-                  child: new Text(
-                    mag.toString(),
-                    style: new TextStyle(color: Colors.black),
-                  ),
+        child:
+            new Text("There is no earthquake data for the specified criteria"),
+      );
+    }
+    var mag = _features[i]['properties']['mag'];
+    return new Card(
+      color: Colors.white,
+      child: new Column(
+        children: <Widget>[
+          new ListTile(
+            leading: new Opacity(
+              opacity: mag / 10,
+              child: new CircleAvatar(
+                backgroundColor: Colors.red[900],
+                child: new Text(
+                  mag.toString(),
+                  style: new TextStyle(color: Colors.black),
                 ),
               ),
-              title: new Text("Magnitude: ${mag.toString()}"),
-              subtitle: new Text(_features[i]['properties']['place']),
             ),
-          ],
-        ),
-      );
-    } else {
-      return new Card();
-    }
+            title: new Text("Magnitude: ${mag.toString()}"),
+            subtitle: new Text(_features[i]['properties']['place']),
+          ),
+        ],
+      ),
+    );
   }
 
   void showMap() {
@@ -186,7 +245,7 @@ class _HomeState extends State<Home> {
 
 Future<Map> getQuakes() async {
   String apiURLquake =
-      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/$_apiType.geojson";
+      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${_apiType}_$_apiRange.geojson";
   http.Response response = await http.get(apiURLquake);
   return json.decode(response.body);
 }
